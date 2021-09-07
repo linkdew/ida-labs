72d4bf3015043d8aefa5f09dbee365fb825d5626
linkdew <luftaesthetics@gmail.com>
2021-09-07 23:09:48 +0300

    random normalized vec gen

--- lab1/main.py
+++ lab1/main.py
@@ -1,16 +1,57 @@
 import matplotlib as plt
+import random
 import numpy as np
 
 
-def randomArray(array, length):
-    for i in range(length):
-        array.append(np.random.rand())
-    return array
+def sum(array):
+    result = 0
+    for i in range(len(array)):
+        result += array[i]
+    return result
+
+
+def setIntValue(value):
+    while not isinstance(value, int):
+        try:
+            value = int(input())
+        except ValueError:
+            print("Enter integer value", end=" ")
+        else:
+            print(value)
+            break
+    return value
+
+
+def generateNormalizedWeightsVector(dim):
+    vector = []
+    norm_var = 0
+    for i in range(dim - 1):
+        vector.append(random.uniform(0, 1 - norm_var))
+        norm_var += vector[i]
+    vector.append(1 - norm_var)
+    return vector
 
 
 def main():
-    array = []
-    print(randomArray(array, 10))
+    models_count = None
+    metrics_count = None
+
+    print("Enter number of alternative models (n): ", end=" ")
+    models_count = setIntValue(models_count)
+
+    print("Enter number of quality metrics (m): ", end=" ")
+    metrics_count = setIntValue(metrics_count)
+
+    print(models_count, metrics_count)
+
+    vec = generateNormalizedWeightsVector(metrics_count)
+    print(vec, sum(vec))
 
 
 main()
+
+
+def randomArray(array, length):
+    for _ in range(length):
+        array.append(np.random.rand())
+    return array
